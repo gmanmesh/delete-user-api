6be6a1a7cb2c2f264bc5d1550cedc6ef35ab7a29
gmanmesh <140646198+gmanmesh@users.noreply.github.com>
2025-07-21 20:04:06 -0500

    Update delete-user.js

--- api/delete-user.js
+++ api/delete-user.js
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
-const SUPABASE_URL = process.env.SUPABASE_URL;
-const SERVICE_ROLE_KEY = process.env.SUPABASE_SERVICE_ROLE_KEY;
+const SUPABASE_URL=process.env.SUPABASE_URL;
+const SERVICE_ROLE_KEY=process.env.SUPABASE_SERVICE_ROLE_KEY;
 
 const supabaseAdmin = createClient(SUPABASE_URL, SERVICE_ROLE_KEY);
 
